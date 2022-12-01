297f45c69d0899414338699dc899272083c0beec
OBAME Gad Erwin <116199106+Bisso2022@users.noreply.github.com>
2022-12-01 10:23:57 +0100

    fyyur-project\venv\Lib\site-packages\babel\localtime

--- __init__.py
+++ __init__.py
@@ -1,70 +1,75 @@
-# SPDX-License-Identifier: MIT
+"""
+    babel.localtime
+    ~~~~~~~~~~~~~~~
 
-from attr import (
-    NOTHING,
-    Attribute,
-    Factory,
-    __author__,
-    __copyright__,
-    __description__,
-    __doc__,
-    __email__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
-    assoc,
-    cmp_using,
-    define,
-    evolve,
-    field,
-    fields,
-    fields_dict,
-    frozen,
-    has,
-    make_class,
-    mutable,
-    resolve_types,
-    validate,
-)
-from attr._next_gen import asdict, astuple
+    Babel specific fork of tzlocal to determine the local timezone
+    of the system.
 
-from . import converters, exceptions, filters, setters, validators
+    :copyright: (c) 2013-2022 by the Babel Team.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import sys
+import pytz
+import time
+from datetime import timedelta
+from datetime import tzinfo
+from threading import RLock
+
+if sys.platform == 'win32':
+    from babel.localtime._win32 import _get_localzone
+else:
+    from babel.localtime._unix import _get_localzone
 
 
-__all__ = [
-    "__author__",
-    "__copyright__",
-    "__description__",
-    "__doc__",
-    "__email__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "asdict",
-    "assoc",
-    "astuple",
-    "Attribute",
-    "cmp_using",
-    "converters",
-    "define",
-    "evolve",
-    "exceptions",
-    "Factory",
-    "field",
-    "fields_dict",
-    "fields",
-    "filters",
-    "frozen",
-    "has",
-    "make_class",
-    "mutable",
-    "NOTHING",
-    "resolve_types",
-    "setters",
-    "validate",
-    "validators",
-]
+_cached_tz = None
+_cache_lock = RLock()
+
+STDOFFSET = timedelta(seconds=-time.timezone)
+if time.daylight:
+    DSTOFFSET = timedelta(seconds=-time.altzone)
+else:
+    DSTOFFSET = STDOFFSET
+
+DSTDIFF = DSTOFFSET - STDOFFSET
+ZERO = timedelta(0)
+
+
+class _FallbackLocalTimezone(tzinfo):
+
+    def utcoffset(self, dt):
+        if self._isdst(dt):
+            return DSTOFFSET
+        else:
+            return STDOFFSET
+
+    def dst(self, dt):
+        if self._isdst(dt):
+            return DSTDIFF
+        else:
+            return ZERO
+
+    def tzname(self, dt):
+        return time.tzname[self._isdst(dt)]
+
+    def _isdst(self, dt):
+        tt = (dt.year, dt.month, dt.day,
+              dt.hour, dt.minute, dt.second,
+              dt.weekday(), 0, -1)
+        stamp = time.mktime(tt)
+        tt = time.localtime(stamp)
+        return tt.tm_isdst > 0
+
+
+def get_localzone():
+    """Returns the current underlying local timezone object.
+    Generally this function does not need to be used, it's a
+    better idea to use the :data:`LOCALTZ` singleton instead.
+    """
+    return _get_localzone()
+
+
+try:
+    LOCALTZ = get_localzone()
+except pytz.UnknownTimeZoneError:
+    LOCALTZ = _FallbackLocalTimezone()
