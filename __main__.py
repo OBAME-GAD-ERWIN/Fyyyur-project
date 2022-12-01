61079d3082791d3cf182b4b97d32479a96af5158
OBAME Gad Erwin <116199106+Bisso2022@users.noreply.github.com>
2022-12-01 10:36:41 +0100

    fyyur-project\venv\Lib\site-packages\certifi

--- __main__.py
+++ __main__.py
@@ -1,4 +1,12 @@
-from .config import main
+import argparse
 
-if __name__ == "__main__":
-    main(prog="alembic")
+from certifi import contents, where
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--contents", action="store_true")
+args = parser.parse_args()
+
+if args.contents:
+    print(contents())
+else:
+    print(where())
