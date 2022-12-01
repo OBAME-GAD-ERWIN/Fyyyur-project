4c6b2e784229b51486dbcffd89f617a678f73cc8
OBAME Gad Erwin <116199106+Bisso2022@users.noreply.github.com>
2022-12-01 10:09:57 +0100

    fyyur-project\venv\Lib\site-packages\arrow

--- util.py
+++ util.py
@@ -1,127 +1,117 @@
-# testing/util.py
-# Copyright (C) 2005-2019 the SQLAlchemy authors and contributors
-# <see AUTHORS file>
-#
-# This module is part of SQLAlchemy and is released under
-# the MIT License: http://www.opensource.org/licenses/mit-license.php
-from __future__ import annotations
+"""Helpful functions used internally within arrow."""
 
-import re
-import types
-from typing import Union
+import datetime
+from typing import Any, Optional, cast
 
+from dateutil.rrule import WEEKLY, rrule
 
-def flag_combinations(*combinations):
-    """A facade around @testing.combinations() oriented towards boolean
-    keyword-based arguments.
-
-    Basically generates a nice looking identifier based on the keywords
-    and also sets up the argument names.
-
-    E.g.::
-
-        @testing.flag_combinations(
-            dict(lazy=False, passive=False),
-            dict(lazy=True, passive=False),
-            dict(lazy=False, passive=True),
-            dict(lazy=False, passive=True, raiseload=True),
-        )
+from arrow.constants import (
+    MAX_ORDINAL,
+    MAX_TIMESTAMP,
+    MAX_TIMESTAMP_MS,
+    MAX_TIMESTAMP_US,
+    MIN_ORDINAL,
+)
 
 
-    would result in::
+def next_weekday(
+    start_date: Optional[datetime.date], weekday: int
+) -> datetime.datetime:
+    """Get next weekday from the specified start date.
 
-        @testing.combinations(
-            ('', False, False, False),
-            ('lazy', True, False, False),
-            ('lazy_passive', True, True, False),
-            ('lazy_passive', True, True, True),
-            id_='iaaa',
-            argnames='lazy,passive,raiseload'
-        )
+    :param start_date: Datetime object representing the start date.
+    :param weekday: Next weekday to obtain. Can be a value between 0 (Monday) and 6 (Sunday).
+    :return: Datetime object corresponding to the next weekday after start_date.
 
+    Usage::
+
+        # Get first Monday after epoch
+        >>> next_weekday(datetime(1970, 1, 1), 0)
+        1970-01-05 00:00:00
+
+        # Get first Thursday after epoch
+        >>> next_weekday(datetime(1970, 1, 1), 3)
+        1970-01-01 00:00:00
+
+        # Get first Sunday after epoch
+        >>> next_weekday(datetime(1970, 1, 1), 6)
+        1970-01-04 00:00:00
     """
-    from sqlalchemy.testing import config
-
-    keys = set()
-
-    for d in combinations:
-        keys.update(d)
-
-    keys = sorted(keys)
-
-    return config.combinations(
-        *[
-            ("_".join(k for k in keys if d.get(k, False)),)
-            + tuple(d.get(k, False) for k in keys)
-            for d in combinations
-        ],
-        id_="i" + ("a" * len(keys)),
-        argnames=",".join(keys),
+    if weekday < 0 or weekday > 6:
+        raise ValueError("Weekday must be between 0 (Monday) and 6 (Sunday).")
+    return cast(
+        datetime.datetime,
+        rrule(freq=WEEKLY, dtstart=start_date, byweekday=weekday, count=1)[0],
     )
 
 
-def resolve_lambda(__fn, **kw):
-    """Given a no-arg lambda and a namespace, return a new lambda that
-    has all the values filled in.
-
-    This is used so that we can have module-level fixtures that
-    refer to instance-level variables using lambdas.
-
-    """
-
-    glb = dict(__fn.__globals__)
-    glb.update(kw)
-    new_fn = types.FunctionType(__fn.__code__, glb)
-    return new_fn()
-
-
-def metadata_fixture(ddl="function"):
-    """Provide MetaData for a pytest fixture."""
-
-    from sqlalchemy.testing import config
-    from . import fixture_functions
-
-    def decorate(fn):
-        def run_ddl(self):
-            from sqlalchemy import schema
-
-            metadata = self.metadata = schema.MetaData()
-            try:
-                result = fn(self, metadata)
-                metadata.create_all(config.db)
-                # TODO:
-                # somehow get a per-function dml erase fixture here
-                yield result
-            finally:
-                metadata.drop_all(config.db)
-
-        return fixture_functions.fixture(scope=ddl)(run_ddl)
-
-    return decorate
-
-
-def _safe_int(value: str) -> Union[int, str]:
+def is_timestamp(value: Any) -> bool:
+    """Check if value is a valid timestamp."""
+    if isinstance(value, bool):
+        return False
+    if not isinstance(value, (int, float, str)):
+        return False
     try:
-        return int(value)
-    except:
-        return value
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 
-def testing_engine(url=None, options=None, future=False):
-    from sqlalchemy.testing import config
-    from sqlalchemy.testing.engines import testing_engine
-    from sqlalchemy import __version__
+def validate_ordinal(value: Any) -> None:
+    """Raise an exception if value is an invalid Gregorian ordinal.
 
-    _vers = tuple(
-        [_safe_int(x) for x in re.findall(r"(\d+|[abc]\d)", __version__)]
-    )
-    sqla_1x = _vers < (2,)
+    :param value: the input to be checked
 
-    if not future:
-        future = getattr(config._current.options, "future_engine", False)
+    """
+    if isinstance(value, bool) or not isinstance(value, int):
+        raise TypeError(f"Ordinal must be an integer (got type {type(value)}).")
+    if not (MIN_ORDINAL <= value <= MAX_ORDINAL):
+        raise ValueError(f"Ordinal {value} is out of range.")
 
-    if sqla_1x:
-        kw = {"future": future} if future else {}
-    else:
-        kw = {}
-    return testing_engine(url, options, **kw)
+
+def normalize_timestamp(timestamp: float) -> float:
+    """Normalize millisecond and microsecond timestamps into normal timestamps."""
+    if timestamp > MAX_TIMESTAMP:
+        if timestamp < MAX_TIMESTAMP_MS:
+            timestamp /= 1000
+        elif timestamp < MAX_TIMESTAMP_US:
+            timestamp /= 1_000_000
+        else:
+            raise ValueError(f"The specified timestamp {timestamp!r} is too large.")
+    return timestamp
+
+
+# Credit to https://stackoverflow.com/a/1700069
+def iso_to_gregorian(iso_year: int, iso_week: int, iso_day: int) -> datetime.date:
+    """Converts an ISO week date into a datetime object.
+
+    :param iso_year: the year
+    :param iso_week: the week number, each year has either 52 or 53 weeks
+    :param iso_day: the day numbered 1 through 7, beginning with Monday
+
+    """
+
+    if not 1 <= iso_week <= 53:
+        raise ValueError("ISO Calendar week value must be between 1-53.")
+
+    if not 1 <= iso_day <= 7:
+        raise ValueError("ISO Calendar day value must be between 1-7")
+
+    # The first week of the year always contains 4 Jan.
+    fourth_jan = datetime.date(iso_year, 1, 4)
+    delta = datetime.timedelta(fourth_jan.isoweekday() - 1)
+    year_start = fourth_jan - delta
+    gregorian = year_start + datetime.timedelta(days=iso_day - 1, weeks=iso_week - 1)
+
+    return gregorian
+
+
+def validate_bounds(bounds: str) -> None:
+    if bounds != "()" and bounds != "(]" and bounds != "[)" and bounds != "[]":
+        raise ValueError(
+            "Invalid bounds. Please select between '()', '(]', '[)', or '[]'."
+        )
+
+
+__all__ = ["next_weekday", "is_timestamp", "validate_ordinal", "iso_to_gregorian"]
